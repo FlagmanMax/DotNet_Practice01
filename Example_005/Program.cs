fda293659f833dbce79d8979a4acc22cfee9744e
borodov.m <borodov.m@gmail.com>
2022-09-19 00:35:47 +0300

    Added Example_005

---
+++ Example_005/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+            int Power(int x)
+            {
+                return x*x;
+            }
+
+            Console.WriteLine("x^2 = "+Power(x));
+        }
+    }
+}
