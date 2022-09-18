a47830868d4fb7168300179b196d44b30171d6bd
borodov.m <borodov.m@gmail.com>
2022-09-18 23:00:41 +0300

    Initial commit

---
+++ example_001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value x : ");
+            double inputValue = 0;
+            double square = 0;
+            inputValue = Convert.ToDouble(Console.ReadLine());
+            square = inputValue*inputValue;
+            Console.WriteLine($"{inputValue} * {inputValue} = {square}");
+        }
+    }
+}
+
