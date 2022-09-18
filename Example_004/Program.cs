fd62e6fd5b00f147ddf3d359f93566bb71e6e691
borodov.m <borodov.m@gmail.com>
2022-09-19 00:57:55 +0300

    Added Example_004

---
+++ Example_004/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.Write("Введите число a: ");
+int var1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int var2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число c: ");
+int var3 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+
+if (var1 > max)
+{
+    max = var1;
+}
+
+if (var2 > max)
+{
+    max = var2;
+}
+
+if (var3 > max)
+{
+    max = var3;
+}
+
+Console.WriteLine("max = " +max);
