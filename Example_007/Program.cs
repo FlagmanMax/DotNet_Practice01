5af281221838b71bc8f31fa366e6bce66ee2b7ff
borodov.m <borodov.m@gmail.com>
2022-09-19 00:24:30 +0300

    added Example_007

---
+++ Example_007/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            if ( n<=0)
+            {
+                Console.Write("Ошибка ввода!");  
+            }
+            else
+            {
+                int i = -n;
+                while (i<=n)
+                {
+                    Console.Write(i+" ");
+                    i++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
